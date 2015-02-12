836764460a331518bdd9c1716f5466aee6962359
EDLyonhart <EDLyonhart@gmail.com>
2015-02-12 09:34:54 -0800

    Scraped through. All requirements complete

--- app.rb
+++ app.rb
@@ -25,11 +25,59 @@ get '/' do
 end
 
 # GET /users/new - display a form for making a new user
-#
+get '/users/new' do
+  erb :new
+end
+
+
 # POST /users - create a user based on params from form
-#
+post '/users' do
+  users.push({first: params[:first], last: params[:last], id: id })
+  id+=1
+  redirect to '/'
+end
+
+
 # GET /users/:id - show a user's info by their id, this should display the info in a form
-#
+get '/users/:id' do
+  @user = users[params[:id].to_i - 1]
+
+  erb :edit
+end
+
+
 # PUT /users/:id - update a user's info based on the form from GET /users/:id
-#
+put '/users/:id' do
+  users.each do |user|
+    if user[:id] == params[:id].to_i
+      user[:first] = params[:first]
+      user[:last] = params[:last]
+    end
+  end
+
+  redirect to '/'
+end
+
+
 # DELETE /users/:id - delete a user by their id
+delete '/users/:id' do
+    users.delete_if{|user| user[:id] == params[:id].to_i}
+
+  redirect to '/'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
